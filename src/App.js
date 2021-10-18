e969695a82f730ea39bc47029e17b3c1891d87dc
Atowar Rahman <ahmed.anik76@gmail.com>
2021-10-18 20:19:16 +0600

    updated

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import './App.css';
+import Header from './Components/Shared/Header/Header';
 
 function App() {
   return (
     <div className="App">
-     <p class="line-through ...">The quick brown fox ...</p>
+      <Router>
+        <Header></Header>
+        <Switch>
+          <Route></Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
--- src/Components/Shared/Header/Header.js
+++ src/Components/Shared/Header/Header.js
@@ -1,9 +1,24 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
+import logo from '../../../images/logo/logo.png'
+import './Header.css'
 
 const Header = () => {
     return (
-        <div>
-            
+        <div className=" nav flex border-t-2 border-green-400 mx-10 items-center">
+            <div className="logo flex-none">
+                <img style={{width: "250px"}} src={logo} alt="site logo" />
+            </div>
+            <div className="nav-menu text-end flex-grow text-right">
+                <NavLink to="/home">Home</NavLink>
+                <NavLink to="/about-us">About</NavLink>
+                <NavLink to="/services">Services</NavLink>
+                <NavLink to="/consultants">Consultants</NavLink>
+                <NavLink to="/consultants">Faq</NavLink>
+                <span>name: </span>
+                <button>Login</button>
+            </div>
+
         </div>
     );
 };
